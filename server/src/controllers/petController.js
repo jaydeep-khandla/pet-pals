b794bc129ffb10a8eeab295092dd56dddd87c98e
JaydeepKhandla <jaydeepkhandla01@gmail.com>
2024-07-21 12:01:12 +0530

    server changes

---
+++ server/src/controllers/petController.js
@@ -0,0 +1,34 @@
+const petServices = require("../services/petServices");
+
+exports.getPets = async (req, res) => {
+
+    if (!req.query) {
+        return res.status(400).json({ error: "Query params are required" });
+    }
+
+    try {
+        const { page, limit, filter, sort } = req.query;
+        const pets = await petServices.paginatedPets(page, limit, filter, sort);
+        res.status(200).json(pets);
+    } catch (error) {
+        console.error("getPets error: ", error);
+
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getPet = async (req, res) => {
+
+    if (!req.params.id) {
+        return res.status(400).json({ error: "Pet id is required" });
+    }
+
+    try {
+        const pet = await petServices.getPetByField({ _id: req.params.id });
+        res.status(200).json(pet);
+    } catch (error) {
+        console.error("getPet error: ", error);
+
+        res.status(500).json({ error: error.message });
+    }
+}
---
+++ server/src/models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Pet = require('./Pet');
+
+module.exports = {
+    User,
+    Pet
+}
